72915d5f8b6d8140b217a7e7ce1e27929da8a1ea
Louis Wong <60389209+Badwomannn@users.noreply.github.com>
2022-05-16 17:00:18 +0800

    Update get_cookie.py

--- get_cookie.py
+++ get_cookie.py
@@ -9,7 +9,7 @@ driver.get('https://glados.rocks/console/checkin')
 # 程序打开网页后20秒内手动登陆账户
 time.sleep(20)
 
-with open('D:\\Documents\\Scripts\\glados\\cookies.txt','w') as f:
+with open('cookies.txt','w') as f:
     # save cookies as json
     f.write(json.dumps(driver.get_cookies()))
 
