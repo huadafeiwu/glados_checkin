442607cd980e92b87b4068f6a31e8a2a697ebe52
Badwomannn <60389209+Badwomannn@users.noreply.github.com>
2022-05-16 15:52:55 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 import time
 import json
 import os
@@ -8,12 +7,6 @@ import os
 cookies = os.environ["COOKIE"]
 
 driver = webdriver.Chrome()
-
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--headless')
-browser = webdriver.Chrome(chrome_options=chrome_options)
 
 driver.get('https://glados.rocks/login')
 driver.maximize_window()
