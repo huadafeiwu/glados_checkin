3ca6419ec55192b2315d03a20cf42f48da0bdb0b
Badwomannn <60389209+Badwomannn@users.noreply.github.com>
2022-05-16 14:45:34 +0800

    Update and rename checkin_glados.py to checkin.py

--- checkin.py
+++ checkin.py
@@ -2,19 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import json
+import os
+
+cookies = os.environ["COOKIE"]
 
 driver = webdriver.Chrome()
 
 driver.get('https://glados.rocks/login')
 driver.maximize_window()
 
-with open('D:\Documents\Scripts\glados\cookies.txt','r') as f:
-    # read cookies 
-    cookies_list = json.load(f)
+cookies_list = json.load(cookies)
 
-    for cookie in cookies_list:
-        print(cookie)
-        driver.add_cookie(cookie)
+for cookie in cookies_list:
+    driver.add_cookie(cookie)
 
 driver.refresh()
 
